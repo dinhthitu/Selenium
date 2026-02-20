e2eb9e62483c00d2962fea0c982133a9a4dfd8f9
TuDT32 <dinhthitu3773@gmail.com>
2026-02-20 22:58:41 +0700

    update

--- src/test/java/exercise1_2/ElementsTest.java
+++ src/test/java/exercise1_2/ElementsTest.java
@@ -3,6 +3,7 @@ package exercise1_2;
 import core.base.BaseTest;
 import core.config.ConfigLoader;
 import core.config.ConfigReader;
+import core.driver.DriverManager;
 import locator.exercise1_2.Elements;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -19,52 +20,52 @@ public class ElementsTest extends BaseTest {
 
     @Test
     void verifyMenuLocators() {
-        Assert.assertTrue(driver.findElement(Elements.TEXT_BOX_MENU).isDisplayed());
-        Assert.assertTrue(driver.findElement(Elements.BROKEN_LINKS).isDisplayed());
-        Assert.assertTrue(driver.findElement(Elements.UP_DOWN).isDisplayed());
+        Assert.assertTrue(DriverManager.get().findElement(Elements.TEXT_BOX_MENU).isDisplayed());
+        Assert.assertTrue(DriverManager.get().findElement(Elements.BROKEN_LINKS).isDisplayed());
+        Assert.assertTrue(DriverManager.get().findElement(Elements.UP_DOWN).isDisplayed());
     }
 
     @Test
     void verifyInputFields() {
-        driver.findElement(Elements.TEXT_BOX_MENU).click();
+        DriverManager.get().findElement(Elements.TEXT_BOX_MENU).click();
 
-        Assert.assertTrue(driver.findElement(Elements.USERNAME).isDisplayed());
-        Assert.assertTrue(driver.findElement(Elements.PERMANENT_ADDRESS).isDisplayed());
+        Assert.assertTrue(DriverManager.get().findElement(Elements.USERNAME).isDisplayed());
+        Assert.assertTrue(DriverManager.get().findElement(Elements.PERMANENT_ADDRESS).isDisplayed());
     }
 
     @Test
     void verifyWebTablesLocator() {
-        Assert.assertTrue(driver.findElement(Elements.WEB_TABLES).isDisplayed());
+        Assert.assertTrue(DriverManager.get().findElement(Elements.WEB_TABLES).isDisplayed());
     }
 
     @Test
     void verifyChainedLocator() {
-        driver.findElement(Elements.UPLOAD_FILE).sendKeys("C:\\fakepath\\test.txt");
+        DriverManager.get().findElement(Elements.UPLOAD_FILE).sendKeys("C:\\fakepath\\test.txt");
         Assert.assertTrue(true, "Upload file locator works");
     }
 
     @Test
     void verifyLinks() {
-        driver.findElement(Elements.VALID_LINK).click();
-        driver.navigate().back();
+        DriverManager.get().findElement(Elements.VALID_LINK).click();
+        DriverManager.get().navigate().back();
 
-        driver.findElement(Elements.INVALID_LINK).click();
-        driver.navigate().back();
+        DriverManager.get().findElement(Elements.INVALID_LINK).click();
+        DriverManager.get().navigate().back();
     }
 
     @Test
     void verifyTagNameLocator() {
-        List<WebElement> links = driver.findElements(Elements.ALL_LINKS);
+        List<WebElement> links = DriverManager.get().findElements(Elements.ALL_LINKS);
         Assert.assertTrue(links.size() > 0, "Links should not be empty");
     }
 
     @Test
     void verifyAdvancedXpath() {
-        driver.findElement(Elements.TEXT_BOX_MENU).click();
+        DriverManager.get().findElement(Elements.TEXT_BOX_MENU).click();
 
-        Assert.assertTrue(driver.findElement(Elements.XPATH_CONTAIN).isDisplayed());
-        Assert.assertTrue(driver.findElement(Elements.XPATH_STARTWITHS).isDisplayed());
-        Assert.assertTrue(driver.findElement(Elements.XPATH_BY_TEXT).isDisplayed());
-        Assert.assertTrue(driver.findElement(Elements.XPATH_BY_ID).isDisplayed());
+        Assert.assertTrue(DriverManager.get().findElement(Elements.XPATH_CONTAIN).isDisplayed());
+        Assert.assertTrue(DriverManager.get().findElement(Elements.XPATH_STARTWITHS).isDisplayed());
+        Assert.assertTrue(DriverManager.get().findElement(Elements.XPATH_BY_TEXT).isDisplayed());
+        Assert.assertTrue(DriverManager.get().findElement(Elements.XPATH_BY_ID).isDisplayed());
     }
 }
